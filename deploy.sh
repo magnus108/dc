7fb70d46438e52d145f5d84f7f8235a30ee7fc30
Magnus Møller <magnus108@me.com>
2017-04-27 00:26:03 +0000

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker stop web || true
 docker rm web || true
 docker rmi magnus108docker/dc_web:current || true
 docker tag magnus108docker/dc_web:$CIRCLE_BRANCH magnus108docker/dc_web:current
-docker run --restart always --name web -p 8080:80 -e MYSQL_PASSWORD=$MYSQL_PASSWORD -e MYSQL_USER=$MYSQL_USER -e MYSQL_ADDR=$MYSQL_ADDR -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD --volumes-from web_data -d magnus108docker/dc_web
+docker run --restart always --name web -p 8080:80 -e MYSQL_PASSWORD=$MYSQL_PASSWORD -e MYSQL_USER=$MYSQL_USER -e MYSQL_ADDR=$MYSQL_ADDR -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD --volumes-from web_data -d magnus108docker/dc_web:current
 EOF
 
